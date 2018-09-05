287ebb37026af46f51022188ee5fed20b2af74ab
darioestupinan <dario.estupinan@gmail.com>
2018-09-04 20:33:47 -0500

    adding functions to click in every square

--- src/components/square.js
+++ src/components/square.js
@@ -3,13 +3,23 @@ import React from 'react';
 import '../styles/square.css';
 
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button className="square" onClick={this.sendAlert}>
+                {this.state.value}
             </button>
         );
     }
+
+    sendAlert = () => this.setState({value: 'X'});
 };
 
 export default Square;
